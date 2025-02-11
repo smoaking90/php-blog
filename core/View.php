efe958eaf877878946277478d7247647920e60db
smoaking90 <cwsmoak90@gmail.com>
2025-02-11 16:56:17 +0100

    adding view logic

---
+++ core/View.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Core;
+
+use RuntimeException;
+
+class View{
+    public static function render(string $template, array $data = [], string $layout = null): string{
+        $content = static::renderTemplate(
+            $template,
+            $data
+        );
+
+        return static::renderLayout($layout, $data, $content);
+    }
+
+    protected static function renderTemplate(string $template, array $data): string{
+        extract($data);
+        $path = dirname(__DIR__) . "/../app/Views/$template.php";
+
+        if(!file_exists($path)){
+            throw new RuntimeException("Error: template file not found: $path");
+        }
+
+        ob_start();
+        require $path;
+        return ob_get_clean();
+    }
+
+    protected static function renderLayout(?string $template, array $data, string $content): string{
+        if(null === $template){
+            return $content;
+        }
+
+        extract([...$data, 'content' => $content]);
+        $path = dirname(__DIR__) . "/../app/Views/$template.php";
+
+        if(!file_exists($path)){
+            throw new RuntimeException("Error: layout file not found: $path");
+        }
+
+        ob_start();
+        require $path;
+        return ob_get_clean();
+    }
+}
